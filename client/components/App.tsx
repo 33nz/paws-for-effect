781367362ad4654caace0e3f592bfa0fcb70ef14
Jacob Wingate <162934807+33nz@users.noreply.github.com>
2024-05-06 11:36:52 +1200

    wrapped subtitle in H2

--- client/components/App.tsx
+++ client/components/App.tsx
@@ -1,4 +1,5 @@
 import Dog from './dog.tsx'
+import Subtitles from './subtitles.tsx'
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
         alt="a spinning paw-print"
         src="/images/paw.png"
       />
+      <Subtitles text="Canines using super-canine abilities for social good." />
       <Dog name="Desdemona" breed="Bulldog" superpower="Heat vision" />
       <Dog name="Spot" breed="Fox terrier" superpower="Heat vision" />
       <Dog name="Butch" breed="Cocker spaniel" superpower="Sense of smell" />
---
+++ client/components/subtitles.tsx
@@ -0,0 +1,9 @@
+interface Props {
+  text: string
+}
+
+function Subtitles(props: Props) {
+  return <h2>{props.text}</h2>
+}
+
+export default Subtitles
