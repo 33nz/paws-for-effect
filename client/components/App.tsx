51edb29e2ecac8f4b5fede77692fb88283f7a231
Jacob Wingate <jacob.wingate33@gmail.com>
2024-07-16 15:10:55 +1200

    add changes

--- client/components/App.tsx
+++ client/components/App.tsx
@@ -10,7 +10,7 @@ function App() {
         src="/images/paw.png"
       />
       <Subtitles text="Canines using super-canine abilities for social good." />
-      <Dog name="Desdemona" breed="Bulldog" superpower="Heat vision" />
+      <Dog name="Jake" breed="Bulldog" superpower="Heat vision" />
       <img src="/images/bulldog.png" alt="A bulldog" />
       <Dog name="Spot" breed="hound" superpower="Heat vision" />
       <img src="/images/hound.png" alt="a hound"></img>
